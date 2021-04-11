5040c9aa75410a784dee964004817365ed8ad5c1
kartik70rajesh <kartik70rajesh@yahoo.co.in>
2021-04-11 22:51:28 +0530

    added script for regex check

--- live-chat.py
+++ live-chat.py
@@ -1,22 +1,29 @@
 import pytchat
 import json
+from datetime import datetime,timedelta
 
-VIDEO_ID = "F183_mEw1qg"
+
+VIDEO_ID = "kV1b4rusqBc"
 MESSAGE_STRING = "😂😂😂"
 BASE_URL = "https://www.youtube.com/watch?v="
 BUFFER_SECS = 5
 def create_url(split_time):
-    if len(split_time) > 2 :
-        #split_time[2]-=BUFFER_SECS
-        return BASE_URL+VIDEO_ID+"&t="+split_time[0]+"h"+split_time[1]+"m"+split_time[2]+"s"
-    else :
-        return BASE_URL+VIDEO_ID+"&t="+split_time[0]+"m"+split_time[1]+"s"
+    
+    while len(split_time) < 3 :
+        split_time.insert(0,0)
+    h,m,s = map(int,split_time)
+    dt = datetime(2021,1,1,h,m,s)
+    if h>0 or m>0 or s>=20 :
+        dt= dt-timedelta(seconds=20)
+        
+    return BASE_URL+VIDEO_ID+"&t="+str(dt.hour)+"h"+str(dt.minute)+"m"+str(dt.second)+"s"
 
 chat = pytchat.create(video_id=VIDEO_ID)
 chat_json=""
 chats=[]
 file = open("chat.txt", "w", encoding="utf-8")
 print("--------------DOWNLOADING CHATS-------------")
+#TO do - add ghumnewala progress bar
 while chat.is_alive():
     chat_json=chat.get().json()
     chats = json.loads(chat_json)
