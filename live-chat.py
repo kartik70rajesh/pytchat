b56e4efb73d9dbe69ef6fe369edaba0ce34a1aa6
kartik70rajesh <kartik70rajesh@yahoo.co.in>
2021-04-12 02:00:48 +0530

    added spinner as download animation

--- live-chat.py
+++ live-chat.py
@@ -1,35 +1,47 @@
 import pytchat
 import json
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+from halo import Halo
 
 
-VIDEO_ID = "kV1b4rusqBc"
+# past live - ZmYDoMeWnu4
+#live - Se3hl0YlFNs
+VIDEO_ID = "ZmYDoMeWnu4"
 MESSAGE_STRING = "😂😂😂"
 BASE_URL = "https://www.youtube.com/watch?v="
 BUFFER_SECS = 5
+
+
 def create_url(split_time):
-    
-    while len(split_time) < 3 :
-        split_time.insert(0,0)
-    h,m,s = map(int,split_time)
-    dt = datetime(2021,1,1,h,m,s)
-    if h>0 or m>0 or s>=20 :
-        dt= dt-timedelta(seconds=20)
-        
+
+    while len(split_time) < 3:
+        split_time.insert(0, 0)
+    h, m, s = map(int, split_time)
+    dt = datetime(2021, 1, 1, h, m, s)
+    if h > 0 or m > 0 or s >= 20:
+        dt = dt-timedelta(seconds=20)
+
     return BASE_URL+VIDEO_ID+"&t="+str(dt.hour)+"h"+str(dt.minute)+"m"+str(dt.second)+"s"
 
+
 chat = pytchat.create(video_id=VIDEO_ID)
-chat_json=""
-chats=[]
-file = open("chat.txt", "w", encoding="utf-8")
-print("--------------DOWNLOADING CHATS-------------")
-#TO do - add ghumnewala progress bar
+chat_json = ""
+chats = []
+timed_url = " "
+final_json = ""
+live_file = open("live_chat.txt", "w", encoding="utf-8")
+spinner = Halo(text='Downloading chats', spinner='dots')
+spinner.start()
 while chat.is_alive():
-    chat_json=chat.get().json()
+    chat_json = chat.get().json()
     chats = json.loads(chat_json)
     for msg in chats:
         if MESSAGE_STRING in msg["message"]:
-            timed_url = create_url(msg["elapsedTime"].split(":"))
-            file.write(msg["elapsedTime"]+" "+msg["message"]+" "+timed_url+"\n")
-print("--------------CHATS DOWNLOADED-------------")
-file.close()
+            #timed_url = create_url(msg["elapsedTime"].split(":"))
+            live_file.write(msg["elapsedTime"]+" " +
+                            msg["message"]+" "+timed_url+"\n")
+            live_file.flush()
+spinner.stop()
+
+
+live_file.close()
